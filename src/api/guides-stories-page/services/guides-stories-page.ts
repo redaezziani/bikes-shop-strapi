1b9b3ee06ccb58e1bc285e4c0d07b490c051c75b
redaezziani <klausdev2@gmail.com>
2025-12-12 17:00:09 +0100

    fix:fix and update

--- src/api/guides-stories-page/services/guides-stories-page.ts
+++ src/api/guides-stories-page/services/guides-stories-page.ts
@@ -1,7 +1,5 @@
-/**
- * guides-stories-page service
- */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::guides-stories-page.guides-stories-page' as any);
+export default factories.createCoreService(
+  'api::guides-stories-page.guides-stories-page' as any,
+);
